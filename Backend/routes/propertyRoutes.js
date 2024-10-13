fde986e4c7704ff00db5f7c34a35330e5d5f6169
amt-francis-amos <sugartone45@yahoo.com>
2024-10-13 13:17:08 -0800

    update

--- Backend/routes/propertyRoutes.js
+++ Backend/routes/propertyRoutes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 const Property = require('../models/Property');
-const Agent = require('../models/Agent'); // Import the Agent model
+
 
 const router = express.Router();
 
@@ -57,33 +57,6 @@ router.post(
   }
 );
 
-// Get route to fetch a single property by ID with agent details
-router.get('/:id', async (req, res) => {
-  try {
-    const property = await Property.findById(req.params.id).populate('agent');
-    if (!property) {
-      return res.status(404).json({ message: 'Property not found' });
-    }
-    res.status(200).json({ property }); // Make sure to return the property with agent details
-  } catch (error) {
-    console.error('Error fetching property:', error.message);
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
-
-
-
-
-// Get route to fetch all properties
-router.get('/', async (req, res) => {
-  try {
-    const properties = await Property.find().populate('agent'); // Optionally populate agent details for all properties
-    res.status(200).json(properties);
-  } catch (error) {
-    console.error('Error fetching properties:', error.message);
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
 
 // Delete route to remove a property and its associated media files
 router.delete('/:id', async (req, res) => {
