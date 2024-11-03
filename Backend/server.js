522962e18cd870f1af47a5c592b8fa266d6ec6f1
amt-francis-amos <sugartone45@yahoo.com>
2024-11-03 12:59:12 -0800

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -6,7 +7,6 @@ const authRoutes = require('./routes/authRoutes');
 const propertyRoutes = require('./routes/propertyRoutes'); 
 const bookingRoutes = require('./routes/bookingRoutes');
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
