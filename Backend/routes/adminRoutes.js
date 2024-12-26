30e5c888d99db319b1e928cecca66bdf8ed67bbc
amt-francis-amos <sugartone45@yahoo.com>
2024-12-25 23:31:59 -0800

    update

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const authenticateToken = require('../middleware/auth.middleware.js');  
 
 
-router.get("/admin/dashboard", authenticateToken, (req, res) => {
+router.get("/dashboard", authenticateToken, (req, res) => {
   res.json({ message: "Welcome to the admin dashboard!" });
 });
+
+
 
 // Other admin routes
 router.get("/admin/users", authenticateToken, (req, res) => {
   res.json({ message: "Manage users" });
 });
-
 
 // Other routes...
 module.exports = router;
