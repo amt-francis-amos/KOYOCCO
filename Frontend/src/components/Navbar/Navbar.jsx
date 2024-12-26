a66e1027a6e625b351e25b563e4973f6976e9cda
amt-francis-amos <sugartone45@yahoo.com>
2024-12-26 03:56:04 -0800

    update

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -235,6 +235,49 @@ const Navbar = () => {
               </li>
             </ul>
           </li>
+ {/* Property Sales Dropdown */}
+ <li className="relative"  onMouseLeave={() => handleMouseLeave('propertySales')} 
+>
+            <button
+              className="hover:text-gray-700 py-2 lg:py-0 flex items-center focus:outline-none"
+              onClick={() => toggleDropdown('propertySales')}
+            >
+              <span className="flex items-center">
+                Property Sales <FaChevronDown size={13} className="ml-2" />
+              </span>
+            </button>
+            {dropdownOpen.propertySales && (
+              <ul className="absolute left-0 mt-1 bg-white shadow-lg rounded-md py-2 w-48 transition-opacity ease-in-out duration-300">
+                <li>
+                  <Link
+                    to="/houses"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                    onClick={closeMenuOnLinkClick}
+                  >
+                    Houses
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/land"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                    onClick={closeMenuOnLinkClick}
+                  >
+                    Land
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/commercial"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                    onClick={closeMenuOnLinkClick}
+                  >
+                    Commercial
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
 
           {/* Other Links */}
           <li>
