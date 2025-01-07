cb9af5e154185a09fd064b2f485cad1577729bb0
amt-francis-amos <sugartone45@yahoo.com>
2025-01-07 12:51:06 -0800

    update

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -3,6 +3,6 @@ const { loginAdmin } = require("../controllers/adminController");
 
 const router = express.Router();
 
-router.post("/register", loginAdmin);
+router.post("/login", loginAdmin);
 
 module.exports = router;
