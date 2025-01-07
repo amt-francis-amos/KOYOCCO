29413341a384be5950fbfd340f89111ac7fabffd
amt-francis-amos <sugartone45@yahoo.com>
2025-01-07 13:41:02 -0800

    update

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,30 +3,93 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');  
 
-;
 
-// Admin login
-const loginAdmin = async (req, res) => {
+//---- API to register user
+
+const registerUser = async (req, res) =>{
+         
   try {
-    const { email, password } = req.body;
 
-    // Check for admin credentials
-    if (
-      email === process.env.ADMIN_EMAIL &&
-      password === process.env.ADMIN_PASSWORD
-    ) {
-      // If valid, generate and return a token
-      const token = jwt.sign({ email }, process.env.JWT_SECRET);
-      return res.json({ success: true, message: token });
-    } else {
-      return res.json({ success: false, message: "Invalid Credentials" });
-    }
+      const {name, email, password} = req.body;
+
+      if(!name || !email || !password){
+          return res.json({success: false, message: 'Missing Details'})
+      }
+
+      // validating email format
+      if(!validator.isEmail(email)){
+          return res.json({success: false, message: 'enter a valid email'})
+      }
+      
+      // validating strong password
+      if(password.length < 8){
+          return res.json({success: false, message: 'enter a strong password'})
+      }
+
+      // hashing user password
+      const salt = await bycrpt.genSalt(10)
+
+      const hashedPassword = await bycrpt.hash(password, salt)
+
+      // save user to database
+      const userData = {
+          name,
+          email,
+          password: hashedPassword
+      }
+
+      const newUser = new userModel(userData)
+
+      const user = await newUser.save()
+
+
+      const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+      res.json({success: true, message: 'User registered successfully', token})
+     
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: error.message });
+      console.log(error);
+      res.json({ success: false, message: error.message });
   }
-};
+}
+
+
+//---- API to  UserLogin
+
+const loginUser = async (req, res) => {
+
+try {
+
+  const {email, password} = req.body;
+
+  const user  = await userModel.findOne({ email})
+  
+ if(!user){
+  return res.json({success: false, message: 'User does not exist'})
+    
+ }
+
+ const isMatch = await bycrpt.compare(password, user.password)
+
+ if(isMatch){
+  const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+  return res.json({success:true, token})
+ }else{
+  return res.json({success: false, message: 'Invalid credentials'})
+ }
 
 
 
-export { loginAdmin};
+
+} catch (error) {
+  console.log(error);
+  res.json({ success: false, message: error.message });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+}
+
+}
+
+
+
+
+
+export {registerUser, loginUser};
