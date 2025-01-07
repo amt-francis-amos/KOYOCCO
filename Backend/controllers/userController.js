ee4cbec51b78e2f8731619e93566411acdfe75e0
amt-francis-amos <sugartone45@yahoo.com>
2025-01-07 14:20:38 -0800

    update

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,7 @@
 
 const bcrypt = require('bcrypt');  
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');  
+const user = require('../models/User');  
 
 
 //---- API to register user
