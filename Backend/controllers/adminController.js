d30f58335d4ff979a62341a54687682d751883a3
amt-francis-amos <sugartone45@yahoo.com>
2025-01-07 12:09:24 -0800

    update

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -40,7 +40,7 @@ const loginAdmin = async (req, res) => {
     });
   } catch (error) {
     console.error("Login error:", error);
-    res.status(500).json({ success: false, message: "Server Error" });
+    res.json({ success: false, message: "Server Error" });
   }
 };
 
