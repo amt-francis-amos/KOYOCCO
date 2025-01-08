02080d22d62aff5ad8cb2fb98a132856b1aabf44
amt-francis-amos <sugartone45@yahoo.com>
2025-01-07 21:30:45 -0800

    updated

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -18,10 +18,10 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border border-b bg-white'>
         <div className='flex items-center gap-2 text-xs'>
-            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+            <img className='w-36 sm:w-20 cursor-pointer' src={assets.koyoccoLogo} alt="" />
             <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
         </div>
-        <button onClick={Logout} className='bg-primary px-10 py-2 text-sm text-white rounded-full'>Logout</button>
+        <button onClick={Logout} className='bg-red-500 px-10 py-2 text-sm text-white rounded-full'>Logout</button>
     </div>
   )
 }
