087f8e97f192083d9febd2ac684407542e326438
amt-francis-amos <sugartone45@yahoo.com>
2025-01-07 17:43:24 -0800

    updated

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -3,14 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const { loginAdmin, registerUser, loginUser } = require("../controllers/adminController");
-const verifyToken = require('../middleware/authAdmin')
+const authenticateToken  = require('../middleware/auth.middleware')
 
 
 // Register a new user
 router.post('/signup', registerUser);
 
 // Login user
-router.post('/login', verifyToken, loginAdmin, loginUser);
+router.post('/login', authenticateToken , loginAdmin, loginUser);
 
 
 module.exports = router;
