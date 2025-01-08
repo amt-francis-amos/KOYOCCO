62d8867431b1ecb15a205bfb198cfdba8d3ea116
amt-francis-amos <sugartone45@yahoo.com>
2025-01-07 21:11:26 -0800

    updated

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   
     try {
       const response = await axios.post(
-        "https://koyocco-backend.onrender.com/api/admin/login",
+        "https://koyocco-backend.onrender.com/api/auth/login",
         { email, password } 
       );
   
