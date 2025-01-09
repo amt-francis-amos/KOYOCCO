615dcfae2db2d573f42adf5857b500156478f302
amt-francis-amos <sugartone45@yahoo.com>
2025-01-09 12:34:34 -0800

    updated

--- Frontend/src/pages/CreateRequest/CreateRequest.jsx
+++ Frontend/src/pages/CreateRequest/CreateRequest.jsx
@@ -152,7 +152,7 @@ const CreateRequest = () => {
         <button
           type="submit"
           disabled={isSubmitting}
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-red-500"
+          className="w-full bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-black"
         >
           {isSubmitting ? 'Submitting...' : 'Submit Request'}
         </button>
