56c62cb589a99ae87c86e3509490d7ec529df3ea
amt-francis-amos <sugartone45@yahoo.com>
2025-01-09 11:03:02 -0800

    updated

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -125,7 +125,7 @@ const Home = () => {
                         {prop.description}
                       </p>
                       <p className="text-red-500 font-bold text-sm md:text-base">
-                      \`/{prop.price}
+                      ₵{prop.price}
                       </p>
                       <p className="text-gray-500 text-xs md:text-sm">
                         {prop.location}
