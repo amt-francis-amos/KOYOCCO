103ac6186909da1bc43aac54b457a3c59efad6b5
amt-francis-amos <sugartone45@yahoo.com>
2025-01-09 15:47:34 -0800

    updated

--- Frontend/src/pages/PropertySales/PropertySales.jsx
+++ Frontend/src/pages/PropertySales/PropertySales.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { motion } from "framer-motion";
 
 const PropertySales = () => {
   const [isPropertyOwner, setIsPropertyOwner] = useState(true);
@@ -116,11 +115,7 @@ const PropertySales = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 py-10">
-      <motion.div className="container mx-auto px-6">
-      initial={{opacity:0, y:100}}
-        transition={{duration:1.5}}
-        whileInView={{opacity:1, y:0}}
-        viewport={{once:true}}
+      <div className="container mx-auto px-6">
         <ToastContainer />
         <h1 className="text-4xl font-bold text-center mb-6">Post Property for Sale</h1>
 
@@ -247,7 +242,7 @@ const PropertySales = () => {
             )}
           </div>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 };
