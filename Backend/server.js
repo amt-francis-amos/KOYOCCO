832c6857dd46b09a45353ac22eccb6b810a3a928
amt-francis-amos <sugartone45@yahoo.com>
2025-01-17 05:08:21 -0800

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.use('/api/requests', requestRoutes);
 app.use('/api/post-listing', listingRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', profileRoutes); 
-app.use('/api/contact-agent', contactAgentRoutes); 
+app.use('/api/contact', contactAgentRoutes); 
 
 // Global error handling middleware
 app.use((err, req, res, next) => {
