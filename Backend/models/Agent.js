503b67f43695fcc9010a30eff2e664b4f2a60508
amt-francis-amos <sugartone45@yahoo.com>
2025-01-17 12:25:39 -0800

    updated

--- Backend/models/Agent.js
+++ Backend/models/Agent.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 
 const AgentSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/ }, // Added email regex validation
-  phone: { 
-    type: String, 
-    required: true, 
-    match: /^(?:\+?\d{1,2}\s?)?(?:\(?\d{2,4}\)?[\s\-]?)?\d{6,10}$/ 
-  },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Agent', AgentSchema);
