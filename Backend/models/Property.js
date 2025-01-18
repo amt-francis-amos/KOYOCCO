d141ae1ee4c05dca56007a1b0c6717bb65d2ae80
amt-francis-amos <sugartone45@yahoo.com>
2025-01-17 18:18:49 -0800

    updated

--- Backend/models/Property.js
+++ Backend/models/Property.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
 const PropertySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, default: "" },
+  name: { type: String, required: true }, 
+  description: { type: String },
   price: { type: Number, required: true },
   location: { type: String, required: true },
-  type: { type: String, required: true, enum: ['Apartment', 'House', 'Land', 'Office', 'Other'] }, // Add property type
-  images: [{ type: String }],
-  video: { type: String, default: "" },
-  status: { type: String, enum: ['Available', 'Sold', 'Rented'], default: 'Available' },
-
-  agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
-}, { timestamps: true });
-
-PropertySchema.index({ location: 1, status: 1 });
+  images: [{ type: String }], 
+  video: { type: String }, // 
+  status: { type: String, enum: ['Available', 'Sold', 'Rented'], default: 'Available' }, 
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+  agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' } ,
+ 
+});
 
 module.exports = mongoose.model('Property', PropertySchema);
