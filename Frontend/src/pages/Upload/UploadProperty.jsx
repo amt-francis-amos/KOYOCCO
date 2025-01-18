5c148cc20e75aeb070001573d2e29ad8b1d30fc5
amt-francis-amos <sugartone45@yahoo.com>
2025-01-17 17:58:19 -0800

    updated

--- Frontend/src/pages/Upload/UploadProperty.jsx
+++ Frontend/src/pages/Upload/UploadProperty.jsx
@@ -146,7 +146,7 @@ const UploadProperty = () => {
         </div>
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+          className="w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-black"
         >
           Upload Property
         </button>
