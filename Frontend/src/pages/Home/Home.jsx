fb3863d3c4c79199be570373fc89eb697daa59a3
amt-francis-amos <sugartone45@yahoo.com>
2025-01-17 18:31:12 -0800

    updated

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -130,8 +130,8 @@ const Home = () => {
                       <p className="text-gray-500 text-xs md:text-sm">
                         {prop.location}
                       </p>
-                      <p className="text-gray-700 text-xs md:text-sm mt-2">
-                        Property Type: {prop.propertyType}
+                      <p className="text-red-700 text-2xl font-semibold md:text-sm mt-2">
+                         {prop.propertyType}
                       </p>
                     </div>
                   </div>
