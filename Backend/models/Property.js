b0ffb21be4ce73d849189154cc7867b316a23861
amt-francis-amos <sugartone45@yahoo.com>
2025-01-19 13:19:48 -0800

    updated

--- Backend/models/Property.js
+++ Backend/models/Property.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
 const PropertySchema = new mongoose.Schema({
-  name: { type: String, required: true }, 
+  name: { type: String, required: true },
   description: { type: String },
   price: { type: Number, required: true },
-  location: { type: String, required: true },
-  propertyType: { type: String, required: true },  
+  location: { type: String, required: true }, 
+  address: { type: String, required: true }, 
+  region: { type: String, required: true },
+  condition: { type: String, enum: ['Newly built', 'Innovated', 'Used'], required: true }, 
+  propertyType: { type: String, required: true }, 
   images: [{ type: String }], 
-  video: { type: String }, 
+  video: { type: String },
   status: { type: String, enum: ['Available', 'Sold', 'Rented'], default: 'Available' }, 
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
-  agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' } ,
+  agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' }, 
 });
 
 module.exports = mongoose.model('Property', PropertySchema);
-
