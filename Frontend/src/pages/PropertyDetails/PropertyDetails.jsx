aaa415920b21e7a07ae00c059777d4c8154069c6
amt-francis-amos <sugartone45@yahoo.com>
2025-01-25 17:49:22 -0800

    updated

--- Frontend/src/pages/PropertyDetails/PropertyDetails.jsx
+++ Frontend/src/pages/PropertyDetails/PropertyDetails.jsx
@@ -34,9 +34,9 @@ const PropertyDetails = () => {
     }
 
     try {
-      // Corrected URL here to match the backend API endpoint
+   
       const response = await axios.get(
-        `https://koyocco-backend.onrender.com/api/agent/${propertyDetail.agentId}`
+        `https://koyocco-backend.onrender.com/api/agents/${propertyDetail.agentId}/contact`
       );
       setAgentContact(response.data);
       setShowContact(true);
