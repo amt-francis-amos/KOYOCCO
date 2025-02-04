2f041b9063ddab077d9b58c0d968a123015b518a
amt-francis-amos <sugartone45@yahoo.com>
2025-02-04 07:42:25 -0800

    updated

--- Frontend/src/components/RequestsDashboard/RequestedDashboard.jsx
+++ Frontend/src/components/RequestsDashboard/RequestedDashboard.jsx
@@ -25,7 +25,7 @@ const RequestsDashboard = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md mt-10">
+    <div className="max-w-6xl mx-auto p-6 bg-white rounded-lg shadow-md mt-10">
       <h1 className="text-2xl font-semibold text-center text-gray-800 mb-6">Relocation Requests</h1>
       <table className="w-full table-auto border-collapse">
         <thead>
@@ -35,6 +35,8 @@ const RequestsDashboard = () => {
             <th className="p-2 border-b">Service Type</th>
             <th className="p-2 border-b">Date</th>
             <th className="p-2 border-b">Region</th>
+            <th className="p-2 border-b">Registration No.</th>
+            <th className="p-2 border-b">Driver's Contact</th>
           
           </tr>
         </thead>
@@ -46,6 +48,9 @@ const RequestsDashboard = () => {
               <td className="p-2 border-b">{request.serviceType}</td>
               <td className="p-2 border-b">{request.date}</td>
               <td className="p-2 border-b">{request.region}</td>
+              <td className="p-2 border-b">{request.registrationNumber}</td>
+              <td className="p-2 border-b">{request.driverContact}</td>
+              
              
             </tr>
           ))}
