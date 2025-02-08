33131e7b18981ad1ec91a9b07653ffd7f77f1c7b
amt-francis-amos <sugartone45@yahoo.com>
2025-02-08 01:01:05 -0800

    updated

--- Backend/routes/requestRoutes.js
+++ Backend/routes/requestRoutes.js
@@ -19,7 +19,7 @@ const upload = multer({ storage: storage });
 router.post('/create', upload.array('carImages', 5), async (req, res) => {
   try {
     const {
-      userName, userEmail, phone,   date, location, 
+      userName, userEmail, phone, date, location, 
       carType, description, registrationNumber, region, driverContact,
     } = req.body;
 
