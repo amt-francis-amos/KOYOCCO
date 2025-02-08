632ac528293dd547ae1932e1603d0706238fc4b0
amt-francis-amos <sugartone45@yahoo.com>
2025-02-08 00:28:42 -0800

    updated

--- Backend/models/Request.js
+++ Backend/models/Request.js
@@ -12,7 +12,7 @@ const requestSchema = new mongoose.Schema({
   description: { type: String, required: true },
   registrationNumber: { type: String, required: true },
   region: { type: String, required: true },
-  driverContact: { type: String, rred: true },
+  driverContact: { type: String, required: true },
   carImages: [{ type: String }],  
 });
 
